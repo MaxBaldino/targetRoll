849a6049611d255cd2f41df064a019b071c16cc8
MaxBaldino <43002640+MaxBaldino@users.noreply.github.com>
2019-02-01 08:20:29 -0500

    Update targetRoll.java

--- src/targetRoll/targetRoll.java
+++ src/targetRoll/targetRoll.java
@@ -1,5 +1,32 @@
 package targetRoll;
 
+import java.util.Scanner;
+
 public class targetRoll {
 
+	public static void Main(String []args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Which number would you like to target?");
+		int target = Integer.parseInt(input.nextLine());
+		System.out.println("How many times should it roll?");
+		int rollCount = Integer.parseInt(input.nextLine());
+		int counter = rollCount;
+		int targetNum = 0;
+		
+		while (counter > 1) {
+			rollDice(target);
+			
+			
+		}
+		
+		
+	}
+	
+	public static int rollDice(int target) {
+		int num = Math.random(1, 6);
+		
+		
+	}
 }
